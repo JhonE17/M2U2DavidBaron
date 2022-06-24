a9fb7def6d7928251569c303dee4d1208f23fba3
David Barón <baronavilandavid@gmail.com>
2022-06-24 16:03:53 -0500

    index.js : añadido eventListener al botón y creadas variables A, B, C y D

---
+++ Actividad 2 ejercicio 2/index.js
@@ -0,0 +1,12 @@
+let boton = document.getElementById("form--button");
+boton.addEventListener('click', () => {
+    let A,B,C,D;
+    A = document.getElementById("form--valor1").value;
+    console.log(A)
+    B = document.getElementById("form--valor2").value;
+    console.log(B)
+    C = document.getElementById("form--valor3").value;
+    console.log(C)
+    D = document.getElementById("form--valor4").value;
+    console.log(D)
+})
