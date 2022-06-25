aceb8a45a32b3999254b301ca50d49b71685dfe9
David Barón <baronavilandavid@gmail.com>
2022-06-25 08:08:18 -0500

    index.js: añadida la lógica del programa

--- Actividad 2 ejercicio 2/index.js
+++ Actividad 2 ejercicio 2/index.js
@@ -1,12 +1,48 @@
 let boton = document.getElementById("form--button");
 boton.addEventListener('click', () => {
+
+    let minMax = (num) => {
+        if(num < minNum){
+            minNum = num;
+        }
+    
+        else if(num > maxNum)
+        {
+            maxNum = num; 
+        }
+    }
+
     let A,B,C,D;
+    let maxNum,minNum;
     A = document.getElementById("form--valor1").value;
-    console.log(A)
+    maxNum = A;
+    minNum = A;
+
     B = document.getElementById("form--valor2").value;
-    console.log(B)
+    minMax(B)
+    
+    
+
     C = document.getElementById("form--valor3").value;
-    console.log(C)
+    minMax(C)
+   
     D = document.getElementById("form--valor4").value;
-    console.log(D)
+    minMax(D)
+
+    let array1 = [A,B,C,D];
+
+
+    array1.forEach((elem, index) => {
+        for(let element of array1){
+            index++
+            if(element == array1[index]){
+                alert("Hay valores iguales");
+                break
+            }
+        }    
+    });
+    
+    
+
+    alert(`El número mayor es ${maxNum} y el número menor es ${minNum}`)
 })
