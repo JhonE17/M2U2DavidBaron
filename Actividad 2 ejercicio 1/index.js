6b8d742781ad6676144b2cd6872ac950b83b3029
David Barón <baronavilandavid@gmail.com>
2022-06-25 12:41:18 -0500

    Añadidos los estilos y mejorada la lógica del ejercicio 1

--- Actividad 2 ejercicio 1/index.js
+++ Actividad 2 ejercicio 1/index.js
@@ -3,14 +3,19 @@ const boton = document.querySelector("#form--button");
 boton.addEventListener('click', () => {
     let nombre = document.getElementById("form--nombre").value;
     let cantidadMaterias = document.getElementById("form--materias").value;
-    const valorMaterias = 3000;
+    const valorMaterias = 300000;
     const valorPapeleria = 28000;
     let valorTotal;
-
-    valorTotal = (cantidadMaterias * valorMaterias)*0.80;
-    valorTotal += valorPapeleria;
-
-    alert(`El estudiante ${nombre} registró la cantidad de materias de ${cantidadMaterias}, y el valor de estas materias es de ${valorTotal}`)
+    if(cantidadMaterias != 0){
+        valorTotal = (cantidadMaterias * valorMaterias)*0.80;
+        valorTotal += valorPapeleria;
+        alert(`El estudiante ${nombre} registró la cantidad de materias de ${cantidadMaterias}, y el valor de estas materias es de ${valorTotal}`)
+    }
+    
+    else{
+        alert("No ingresó ninguna materia!s")
+    }
+    
 });
 
 
