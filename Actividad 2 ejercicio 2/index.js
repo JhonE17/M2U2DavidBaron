3b6f18b3b2db3503c70a0c245977a88a632e05d5
David Barón <baronavilandavid@gmail.com>
2022-06-29 19:10:40 -0500

    hotfixeado error presentado anteriormente en el push del array

--- Actividad 2 ejercicio 2/index.js
+++ Actividad 2 ejercicio 2/index.js
@@ -31,14 +31,14 @@ boton.addEventListener('click', () => {
     let a,b,c,d;
     let array1 = [] // 2,234,3,43
     a = document.querySelector("#form--valor1").value;
-    array1.push(a) 
+    array1.push(parseInt(a)) 
     b = document.querySelector("#form--valor2").value;
-    array1.push(b)
+    array1.push(parseInt(b))
     c = document.querySelector("#form--valor3").value;
-    array1.push(c)
+    array1.push(parseInt(c))
     d = document.querySelector("#form--valor4").value;
     // console.log(d)
-    array1.push(d)
+    array1.push(parseInt(d))
     let max = 0;
     maxNumChecker();
     let min = max;
